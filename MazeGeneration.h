63d58cd7d2f50c5f9b17d9c54d205f895470472b
Daniyal Faraz <daniyalfaraz2003@gmail.com>
2024-05-27 20:02:42 +0500

    dfs maze generation is now complete

--- MazeGeneration.h
+++ MazeGeneration.h
@@ -33,18 +33,24 @@ namespace MazeGenerationAlgorithms {
 				Cell(int i, int j) : i(i), j(j) {}
 			};
 
-			bool isValidCell(Cell& cell) {
+			static bool isValidCell(Cell& cell) {
 				return cell.i >= 1 && cell.i < (MAZE_HEIGHT - 1)
 					&& cell.j >= 1 && cell.j < (MAZE_WIDTH - 1);
 			}
 
-			Cell generateRandomCell() {
+			static Cell generateRandomCell(vector<vector<char>>& maze) {
 				srand(time(NULL));
 				Cell result(-1, -1);
-				
+				result.i = 1 + rand() % (MAZE_HEIGHT - 1);
+				result.j = 1 + rand() % (MAZE_WIDTH - 1);
+				while (maze[result.i][result.j] == '#') {
+					result.i = 1 + rand() % (MAZE_HEIGHT - 1);
+					result.j = 1 + rand() % (MAZE_WIDTH - 1);
+				}
+				return result;
 			}
 
-			vector<Cell> getAdjCells(Cell& cell, bool visited[][MAZE_WIDTH]) {
+			static vector<Cell> getAdjCells(Cell& cell, bool visited[][MAZE_WIDTH]) {
 				Cell up = Cell(cell.i - 2, cell.j);
 				Cell down = Cell(cell.i + 2, cell.j);
 				Cell left = Cell(cell.i, cell.j - 2);
@@ -78,8 +84,35 @@ namespace MazeGenerationAlgorithms {
 				}
 
 				bool visited[MAZE_HEIGHT][MAZE_WIDTH]{};
-				
-
+				Cell start = generateRandomCell(maze);
+				visited[start.i][start.j] = true;
+				st.push(start);
+				while (!st.empty()) {
+					Cell current = st.top();
+					st.pop();
+					vector<Cell> adjacentCells = getAdjCells(current, visited);
+					if (adjacentCells.size() > 0) { // this means valid neighbors exist
+						st.push(current);
+						int choice = rand() % adjacentCells.size();
+						Cell selected = adjacentCells[choice];
+						int dx = selected.j - current.j;
+						int dy = selected.i - current.i;
+						if (dy == 0 && dx > 0) { // the right cell
+							maze[current.i][current.j + 1] = ' ';
+						}
+						else if (dy > 0 && dx == 0) { // the down cell
+							maze[current.i + 1][current.j] = ' ';
+						}
+						else if (dy < 0 && dx == 0) { // the up cell
+							maze[current.i - 1][current.j] = ' ';
+						}
+						else if (dy == 0 && dx < 0) { // the left cell
+							maze[current.i][current.j - 1] = ' ';
+						}
+						visited[selected.i][selected.j] = true;
+						st.push(selected);
+					}
+				}
 			}
 		};
 
