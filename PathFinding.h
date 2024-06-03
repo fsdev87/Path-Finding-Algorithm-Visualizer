38441acdee58ae4490fc22627c756339b3c58475
Daniyal Faraz <daniyalfaraz2003@gmail.com>
2024-06-03 15:54:51 +0500

    dijkstra done

--- PathFinding.h
+++ PathFinding.h
@@ -22,7 +22,7 @@ namespace PathFindingAlgorithms {
 		}
 
 		void findPathDijkstra(vector<vector<char>>& maze, pair<int, int> start, pair<int, int> end) {
-
+			dijkstra.findPath(maze, start, end);
 		}
 
 		void findPathAStar(vector<vector<char>>& maze, pair<int, int> start, pair<int, int> end) {
@@ -256,7 +256,136 @@ namespace PathFindingAlgorithms {
 		};
 
 		class Dijkstra {
+		private:
+			struct Cell {
+				int i, j;
+				Cell(int i = 0, int j = 0) : i(i), j(j) {}
+				bool operator==(const Cell& other) const {
+					return i == other.i && j == other.j;
+				}
 
+				bool operator<(const Cell& other) const {
+					return std::tie(i, j) < std::tie(other.i, other.j);
+				}
+			};
+
+			struct CellPriority {
+				Cell cell;
+				int priority;
+
+				bool operator>(const CellPriority& other) const {
+					return priority > other.priority;
+				}
+			};
+
+			bool initialized;
+			priority_queue<CellPriority, vector<CellPriority>, greater<CellPriority>> pq;
+			vector<Cell> path;
+			map<Cell, Cell> parentMap;
+			map<Cell, int> distances;
+
+			bool isValid(Cell& cell) {
+				return cell.i >= 1 && cell.i < (MAZE_HEIGHT - 1)
+					&& cell.j >= 1 && cell.j < (MAZE_WIDTH - 1);
+			}
+
+			void reconstructPath(Cell end, Cell start) {
+				path.clear();
+				for (Cell at = end; !(at == start); at = parentMap[at]) {
+					path.push_back(at);
+				}
+				std::reverse(path.begin(), path.end());
+			}
+
+			vector<Cell> getAdjNodes(vector<vector<char>>& maze, Cell& cell) {
+				Cell up = Cell(cell.i - 1, cell.j);
+				Cell down = Cell(cell.i + 1, cell.j);
+				Cell left = Cell(cell.i, cell.j - 1);
+				Cell right = Cell(cell.i, cell.j + 1);
+
+				vector<Cell> cells;
+				if (isValid(up)) {
+					char c = maze[up.i][up.j];
+					if (c == ' ' || c == 'C' || c == 'E') {
+						cells.push_back(up);
+					}
+				}
+				if (isValid(down)) {
+					char c = maze[down.i][down.j];
+					if (c == ' ' || c == 'C' || c == 'E') {
+						cells.push_back(down);
+					}
+				}
+				if (isValid(left)) {
+					char c = maze[left.i][left.j];
+					if (c == ' ' || c == 'C' || c == 'E') {
+						cells.push_back(left);
+					}
+				}
+				if (isValid(right)) {
+					char c = maze[right.i][right.j];
+					if (c == ' ' || c == 'C' || c == 'E') {
+						cells.push_back(right);
+					}
+				}
+				return cells;
+			}
+
+
+			void initialize(pair<int, int> start, pair<int, int> end) {
+				Cell startCell = Cell(start.first, start.second);
+				pq.push({ startCell, 0 });
+				distances[startCell] = 0;
+				parentMap[startCell] = startCell;
+				initialized = true;
+			}
+
+		public:
+			Dijkstra() {
+				initialized = false;
+			}
+
+			void findPath(vector<vector<char>>& maze, pair<int, int> start, pair<int, int> end) {
+				if (!initialized) initialize(start, end);
+				if (pq.empty()) {
+					return;
+				}
+				CellPriority currentPriority = pq.top();
+				Cell current = currentPriority.cell;
+				pq.pop();
+				if (maze[current.i][current.j] != 'S' && maze[current.i][current.j] != 'E') {
+					maze[current.i][current.j] = 'C';
+				}
+				if (maze[current.i][current.j] == 'E') {
+					// then we have found the path
+					reconstructPath(current, Cell(start.first, start.second));
+					while (!pq.empty()) {
+						pq.pop();
+					}
+					for (Cell& cell : path) {
+						if (cell == current) continue;
+						maze[cell.i][cell.j] = 'R';
+					}
+					return;
+				}
+
+				vector<Cell> adjCells = getAdjNodes(maze, current);
+				for (Cell& next : adjCells) {
+					int newDist = distances[current] + 1;
+					if (distances[next]) {
+						if (newDist < distances[next]) {
+							distances[next] = newDist;
+							parentMap[next] = current;
+							pq.push({ next, newDist });
+						}
+					}
+					else {
+						distances[next] = newDist;
+						parentMap[next] = current;
+						pq.push({ next, newDist });
+					}
+				}
+			}
 		};
 
 		class AStar {
