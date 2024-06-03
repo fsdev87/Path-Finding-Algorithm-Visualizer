54cdb7bca4d2eb95fc25bafdb5016b03683e5b2e
Daniyal Faraz <daniyalfaraz2003@gmail.com>
2024-06-03 15:16:26 +0500

    bfs done

--- PathFinding.h
+++ PathFinding.h
@@ -18,7 +18,7 @@ namespace PathFindingAlgorithms {
 		}
 
 		void findPathBFS(vector<vector<char>>& maze, pair<int, int> start, pair<int, int> end) {
-
+			bfs.findPath(maze, start, end);
 		}
 
 		void findPathDijkstra(vector<vector<char>>& maze, pair<int, int> start, pair<int, int> end) {
@@ -143,7 +143,116 @@ namespace PathFindingAlgorithms {
 		};
 
 		class BFS {
+		private:
+			struct Cell {
+				int i, j;
+				Cell(int i = 0, int j = 0) : i(i), j(j) {}
+				bool operator==(const Cell& other) const {
+					return i == other.i && j == other.j;
+				}
 
+				bool operator<(const Cell& other) const {
+					return std::tie(i, j) < std::tie(other.i, other.j);
+				}
+			};
+			bool initialized;
+			queue<Cell> q;
+			vector<Cell> path;
+			bool visited[MAZE_HEIGHT][MAZE_WIDTH]{};
+			map<Cell, Cell> parentMap;
+
+			bool isValid(Cell& cell) {
+				return cell.i >= 1 && cell.i < (MAZE_HEIGHT - 1)
+					&& cell.j >= 1 && cell.j < (MAZE_WIDTH - 1);
+			}
+
+			void reconstructPath(Cell end, Cell start) {
+				path.clear();
+				for (Cell at = end; !(at == start); at = parentMap[at]) {
+					path.push_back(at);
+				}
+				std::reverse(path.begin(), path.end());
+			}
+
+			vector<Cell> getAdjNodes(vector<vector<char>>& maze, Cell& cell) {
+				Cell up = Cell(cell.i - 1, cell.j);
+				Cell down = Cell(cell.i + 1, cell.j);
+				Cell left = Cell(cell.i, cell.j - 1);
+				Cell right = Cell(cell.i, cell.j + 1);
+
+				vector<Cell> cells;
+				if (isValid(up)) {
+					char c = maze[up.i][up.j];
+					if (c == ' ' || c == 'C' || c == 'E') {
+						cells.push_back(up);
+					}
+				}
+				if (isValid(down)) {
+					char c = maze[down.i][down.j];
+					if (c == ' ' || c == 'C' || c == 'E') {
+						cells.push_back(down);
+					}
+				}
+				if (isValid(left)) {
+					char c = maze[left.i][left.j];
+					if (c == ' ' || c == 'C' || c == 'E') {
+						cells.push_back(left);
+					}
+				}
+				if (isValid(right)) {
+					char c = maze[right.i][right.j];
+					if (c == ' ' || c == 'C' || c == 'E') {
+						cells.push_back(right);
+					}
+				}
+				return cells;
+			}
+
+
+			void initialize(pair<int, int> start, pair<int, int> end) {
+				Cell startCell = Cell(start.first, start.second);
+				q.push(startCell);
+				initialized = true;
+			}
+
+		public:
+			BFS() {
+				initialized = false;
+			}
+
+			void findPath(vector<vector<char>>& maze, pair<int, int> start, pair<int, int> end) {
+				if (!initialized) initialize(start, end);
+				if (q.empty()) {
+					return;
+				}
+
+				Cell current = q.front();
+				q.pop();
+				if (maze[current.i][current.j] != 'S' && maze[current.i][current.j] != 'E') {
+					maze[current.i][current.j] = 'C';
+				}
+				if (maze[current.i][current.j] == 'E') {
+					// then we have found the path
+					reconstructPath(current, Cell(start.first, start.second));
+					while (!q.empty()) {
+						q.pop();
+					}
+					for (Cell& cell : path) {
+						if (cell == current) continue;
+						maze[cell.i][cell.j] = 'R';
+					}
+					return;
+				}
+
+				vector<Cell> adjCells = getAdjNodes(maze, current);
+				for (Cell& next : adjCells) {
+					if (visited[next.i][next.j] == false) {
+						visited[next.i][next.j] = true;
+						parentMap[next] = current;
+						q.push(next);
+					}
+				}
+			}
 		};
 
 		class Dijkstra {
